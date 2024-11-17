3db181a12772b142ae649feadb96f4c3c77df882
Matheus Hensley <matheushensley_25@hotmail.com>
2024-11-17 16:24:12 -0300

    corrections on setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="ASimpleNeuralNetworkLib",
     version="0.1",
-    packages=find_packages(where='simple_nn'),
+    packages=find_packages(where=".", include=["simple_nn", "simple_nn.*"]),
     install_requires=[
         "numpy"
     ],
