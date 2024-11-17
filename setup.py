72240a267ffab85fec63d27615dc8b411923d1ad
Matheus Hensley <matheushensley_25@hotmail.com>
2024-11-17 16:13:01 -0300

    updates to project structure

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="ASimpleNeuralNetworkLib",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(where='simple_nn'),
     install_requires=[
         "numpy"
     ],
